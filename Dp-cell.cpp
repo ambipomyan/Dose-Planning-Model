c0ed2e3423e275997481e1a1bd41a37f94d14483
Kewei Yan <ambipom_yan@outlook.com>
2021-09-03 00:46:48 -0400

    Update dose applying functions
    - Add if clauses to simulate medicinal effect on different cells: core, fluid, and normal
    - Add stencil to simulate cell growth

--- Dp-cell.cpp
+++ Dp-cell.cpp
@@ -24,11 +24,24 @@ int dp_classification(DP_DOMAIN *domain) {
 }
 
 int dp_apply_dose(DP_DOMAIN *domain) {
-    for (int i = 0; i < domain->nrows; i++) {
-        for (int j = 0; j < domain->ncols; j++) {
-            if (i%2==0 && j%2==0) domain->data[i*domain->ncols+j] = \
-                1*domain->ndoses[domain->ntime] + \
-                domain->old_data[i*domain->ncols+j];
+    // dummy dose
+    for (int i = 1; i < domain->nrows-1; i++) {
+        for (int j = 1; j < domain->ncols-1; j++) {
+            // stencil
+            domain->data[i*domain->ncols+j] = (domain->old_data[(i-1)*domain->ncols+j] + \
+                                               domain->old_data[i*domain->ncols+(j-1)] + \
+                                               domain->old_data[i*domain->ncols+(j+1)] + \
+                                               domain->old_data[(i+1)*domain->ncols+j]) / 4;
+            
+            if (i%2==0 && j%2==0) {
+                if (domain->data[i*domain->ncols+j] == 200) {
+                    domain->data[i*domain->ncols+j] = abs(4*domain->ndoses[domain->ntime] + domain->old_data[i*domain->ncols+j]);
+                } else if (domain->data[i*domain->ncols+j] == 100) {
+                    domain->data[i*domain->ncols+j] = abs(3*domain->ndoses[domain->ntime] + domain->old_data[i*domain->ncols+j]);
+                } else {
+                    domain->data[i*domain->ncols+j] = abs(5*domain->ndoses[domain->ntime] + domain->old_data[i*domain->ncols+j]);
+                }
+            }
         }
     }
     
