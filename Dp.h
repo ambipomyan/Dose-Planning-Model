0fb6adfefe3f9e09f08bca85c9e3328eb776c9d1
Kewei Yan <ambipom_yan@outlook.com>
2021-09-02 20:23:29 -0400

    Update prototype of data structure and methods
    - Set dose to a function of max_time, however, it could be changed later

--- Dp.h
+++ Dp.h
@@ -3,3 +3,31 @@
 
 #include <string.h>
 #include <math.h>
+
+typedef struct DP_DOMAIN_ {
+    // size
+    int nrows;
+    int ncols;
+    // data
+    int *data; // size of nrows*ncols
+    int *ndoses; // size of max_time
+    // constrains
+    int tol;
+    int ntime;
+    int max_time;
+}DP_DOMAIN;
+
+// init
+int dp_init_domain();
+
+int dp_init_dose_plan();
+
+// util
+int ad_print_domain();
+
+// cell
+int dp_classification();
+
+int dp_apply_dose();
+
+int dp_simulation();
